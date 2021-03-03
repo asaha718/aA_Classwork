96920bb36c4f644c628f1a4952957a609d364dd9
alexzhou415 <zhoualex10@yahoo.com>
2021-03-03 15:00:03 -0800

    Finish user controller specs

---
+++ W7D3/GoalApp/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe "GET #index" do 
+    it "renders all users" do
+      get :index
+
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #new" do
+    it "renders new user template" do
+      get :new
+
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "GET #show" do
+    it "renders a specific user" do
+      get :show
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe "POST #create" do
+    let(:valid_params) { {user:  {username: 'mark', password: '123456'}}}
+    let(:user) {User.find_by(username: 'mark')}
+    before :each do
+      post :create, params: valid_params
+    end
+
+    it "creates user with valid params" do
+      expect(User.last.username).to eq("mark")
+    end
+
+    it "logs in the user" do
+      expect(session[:session_token]).to eq(user.session_token)
+    end
+
+    it "redirects to user show page" do
+      expect(response).to redirect_to(user_url(user))
+
+    end
+  end
+end
--- W7D3/GoalApp/spec/models/user_spec.rb
+++ W7D3/GoalApp/spec/models/user_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    subject(:mark) {User.create(username: 'mark', password: '123456')}
+    it {should validate_presence_of(:username)}
+    it {should validate_presence_of(:password_digest)}
+    it {should validate_presence_of(:session_token)}
+    it {should validate_length_of(:password).is_at_least(6)}
+
+    it {should validate_uniqueness_of(:username)}
+  end
 end
