c7e8d1f2e8f644467ed2483a09bb322203dfd6e5
Anug Saha <anugsaha@gmail.com>
2021-03-11 10:17:12 -0500

    practice javascript

---
+++ W8D3/skeleton/phase_1_arrays.js
@@ -0,0 +1,24 @@
+Array.prototype.uniq = function(){
+    let arr = this;
+    let result = [];
+     for (let i = 0; i < arr.length-1; i++){
+       if( !result.includes(arr[i]) ) {
+            result.push(arr[i]);
+       }
+     }
+    return result;
+}
+
+
+Array.prototype.twoSum = function(){
+  let arr = this;
+  let result = [];
+  for (let i = 0; i < arr.length - 1; i++){
+      for (let j = i+1; j < arr.length; j++){
+          if (arr[i] + arr[j] === 0){
+           result.push([i,j]);
+          }
+      }
+  }
+  return result
+}
