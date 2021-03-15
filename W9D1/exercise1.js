495b22d16f6ea5a8bdfeea413ec4fd5f14ff02d8
Anug Saha <anugsaha@gmail.com>
2021-03-15 17:54:23 -0400

    complete args exercise problems

--- W9D1/exercise1.js
+++ W9D1/exercise1.js
@@ -81,3 +81,48 @@ const notMarkovSays = markov.says.myBind(pavlov);
 notMarkovSays("meow", "me");
 // Pavlov says meow to me!
 // true
+
+function curriedSum(numArgs){
+    const nums= [];  
+    return function _curriedSum(num){ 
+        nums.push(num);
+        if (nums.length === numArgs){ 
+            console.log(nums.reduce((acc, ele) => { 
+                return acc + ele; 
+            })); 
+        }
+        return _curriedSum; 
+    }
+}
+
+// const res = curriedSum(4);
+// res(5)(30)(20)(1);
+
+Function.prototype.curry= function(numArgs) {
+    let that= this; 
+    const nums= [];  
+    return function _curriedSum(num){ 
+        nums.push(num);
+        if (nums.length === numArgs){ 
+            // that.apply(null, nums)
+            // that.call(null, ...nums)
+            return that(...nums); 
+        }
+        return _curriedSum; 
+    }
+}
+
+function sumThree(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+console.log(sumThree(4, 20, 6)); // == 30
+
+// you'll write `Function#curry`!
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+f1 = f1(4); // [Function]
+f1 = f1(20); // [Function]
+f1 = f1(6); // = 30
+
+// or more briefly:
+console.log(sumThree.curry(3)(4)(20)(6)); // == 30
