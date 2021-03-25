de7e1028bc68958bd44e9b5679e153dc9e7da1a9
Anug Saha <anugsaha@gmail.com>
2021-03-25 15:13:03 -0400

    setup todo list

---
+++ W10D4/todospt1/frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    const hello = <h1>To Do List</h1>;
+    ReactDOM.render(hello, root);
+  });
